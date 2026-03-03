58f7be118add86e9e2b2947b37696102278f075f
dawitzeleke <dawitzeleke92@gmail.com>
2026-03-03 11:55:33 +0300

    Implement the intial login page design

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 
 class AppColors {
-  static const Color primary = Color.fromRGBO(20, 183, 232, 1);
+  static const Color primary = Color.fromRGBO(20, 183, 232, 0.8);
   static const Color onPrimary = Color.fromRGBO(255, 255, 255, 1);
 }
